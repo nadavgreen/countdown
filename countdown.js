a48ca2388cdd554e3ec309861c3f033544a0ee68
Nadav Green <nadavgreen@Nadavs-MacBook-Pro.local>
2020-01-18 08:47:05 -0500

    project

---
+++ countdown.js
@@ -0,0 +1,23 @@
+var endDate = new Date("01 Jun 2020").getTime();
+
+var timer = setInterval(function() {
+
+    let now = new Date().getTime();
+    let t = endDate - now;
+    
+    if (t >= 0) {
+    
+        let days = Math.floor(t / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+        let secs = Math.floor((t % (1000 * 60)) / 1000);
+    
+        document.getElementById("root").innerHTML = days + "d " + hours + "h " + mins + "m " + secs + "s until Audible";
+
+    } else {
+
+        document.getElementById("root").innerHTML = "The countdown is over!";
+    
+    }
+   
+}, 1000);
