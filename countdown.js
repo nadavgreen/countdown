532ccbae118ddcb58228907a302364671c0bcbc3
Davie <43800418+nadavgreen@users.noreply.github.com>
2020-03-20 12:33:07 -0400

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -27,9 +27,9 @@ var timer = setInterval(function() {
         let lSecs = Math.floor((l / 1000) % 60);
     
         document.getElementById("root").innerHTML =
-            "Audible:" + aDays + "d " + aHours + "h " + aMins + "m " + aSecs + "s ";
-//             + "Meeting:" + mDays + "d " + mHours + "h " + mMins + "m " + mSecs + "s "
-//             + "Last Day:" + lDays + "d " + lHours + "h " + lMins + "m " + lSecs + "s";
+            "Audible: " + aDays + "d " + aHours + "h " + aMins + "m " + aSecs + "s ";
+//             + "Meeting: " + mDays + "d " + mHours + "h " + mMins + "m " + mSecs + "s "
+//             + "Last Day: " + lDays + "d " + lHours + "h " + lMins + "m " + lSecs + "s";
 
     } else {
 
