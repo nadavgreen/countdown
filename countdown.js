790ddab03e6db86cae7b0d2c5973e5cef6a56dab
Davie <43800418+nadavgreen@users.noreply.github.com>
2020-03-20 12:51:54 -0400

    Update countdown.js (#4)

--- countdown.js
+++ countdown.js
@@ -9,28 +9,37 @@ var timer = setInterval(function() {
     let m = meeting - now;
     let l = lastDay - now;
     
-    if (t >= 0) {
+    if (m >= 0) {
     
-        let aDays = Math.floor(a / (1000 * 60 * 60 * 24));
-        let aHours = Math.floor((a / (1000 * 60 * 60)) % 24);
-        let aMins = Math.floor((a / 1000 / 60) % 60);
-        let aSecs = Math.floor((a / 1000) % 60);
-        
         let mDays = Math.floor(m / (1000 * 60 * 60 * 24));
         let mHours = Math.floor((m / (1000 * 60 * 60)) % 24);
         let mMins = Math.floor((m / 1000 / 60) % 60);
         let mSecs = Math.floor((m / 1000) % 60);
+    
+        document.getElementById("root").innerHTML = "Meeting: " + mDays + "d " + mHours + "h " + mMins + "m " + mSecs + "s ";
         
+    }
+    
+    if (l >= 0) {
+
         let lDays = Math.floor(l / (1000 * 60 * 60 * 24));
         let lHours = Math.floor((l / (1000 * 60 * 60)) % 24);
         let lMins = Math.floor((l / 1000 / 60) % 60);
         let lSecs = Math.floor((l / 1000) % 60);
     
-        document.getElementById("root").innerHTML =
-            "Audible: " + aDays + "d " + aHours + "h " + aMins + "m " + aSecs + "s ";
-//             + "Meeting: " + mDays + "d " + mHours + "h " + mMins + "m " + mSecs + "s "
-//             + "Last Day: " + lDays + "d " + lHours + "h " + lMins + "m " + lSecs + "s";
+        document.getElementById("root").innerHTML = "Last Day: " + lDays + "d " + lHours + "h " + lMins + "m " + lSecs + "s ";
+        
+    }
+    
+    if (a >= 0) {
+    
+        let aDays = Math.floor(a / (1000 * 60 * 60 * 24));
+        let aHours = Math.floor((a / (1000 * 60 * 60)) % 24);
+        let aMins = Math.floor((a / 1000 / 60) % 60);
+        let aSecs = Math.floor((a / 1000) % 60);
 
+        document.getElementById("root").innerHTML = "Audible: " + aDays + "d " + aHours + "h " + aMins + "m " + aSecs + "s";
+        
     } else {
 
         document.getElementById("root").innerHTML = "The countdown is over!";
